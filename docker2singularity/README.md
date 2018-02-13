9802acbd032d69a91bf61b9b116234d329696479
vsoch <vsochat@stanford.edu>
2018-02-13 13:20:01 -0800

    first commit

---
+++ docker2singularity/README.md
@@ -0,0 +1,202 @@
+# docker2singularity
+
+This is an inherited container build from [docker2singularity](https://www.github.com/singularityware/docker2singularity) that uses a common base installed with Singularity. See the README.md in that repository for details, and here is the quick usage.
+
+## Build
+
+```
+docker build -t docker2singularity .
+```
+
+## Usage
+
+```
+docker run docker2singularity
+USAGE: docker2singularity [-m "/mount_point1 /mount_point2"] [options] docker_image_name
+OPTIONS:
+
+          Image Format
+              -f: build development sandbox (folder)
+              -w: non-production writable image (ext3)         
+
+              Default is squashfs (recommended)
+
+```
+
+### Options
+
+**Image Format**
+
+ - `squashfs` (no arguments specified) gives you a squashfs (`*.simg`) image. This is a compressed, reliable, and read only format that is recommended for production images. Squashfs support was added to Singularity proper in [January of 2017](https://github.com/singularityware/singularity/commit/0cf00d1251ff276d5b9b7a0e4eadb783a45a6b65#diff-8405d9d311d83f009adff55c3deb112c) and thus available as early as the 2.2.1 release.
+ - `sandbox` (`-f`) builds your image into a sandbox **folder**. This is ideal for development, as it will produce a working image in a folder on your system.
+ - `ext3` (`-w`) builds an older format (ext3) image (`*.img`). This format is not recommended for production images as we have observed degradation of the images over time, and they tend to be upwards of 1.5x to 2x the size of squashfs.
+
+Note that you are able to convert easily from a folder or ext3 image using Singularity 2.4. If your choice is to develop, making changes, and then finalize, this approach is **not** recommended - your changes are not recorded and thus the image not reproducible.
+
+**Mount Points**
+
+ - `-m` specify one or more mount points to create in the image.
+
+**Image Name**
+
+The last argument (without a letter) is the name of the docker image, as you would specify to run with Docker (e.g., `docker run ubuntu:latest`)
+
+
+## Examples
+
+## Build a Squashfs Image
+Squashfs is the recommended image type, it is compressed and less prone to degradation over time. You don't need to specify anything special to create it:
+
+```
+docker run -v /var/run/docker.sock:/var/run/docker.sock \
+-v /host/path/change/me:/output \
+--privileged -t --rm \
+singularityware/docker2singularity
+ubuntu:14.04
+
+Image Format: squashfs
+Inspected Size: 188 MB
+
+(1/10) Creating a build sandbox...
+(2/10) Exporting filesystem...
+(3/10) Creating labels...
+(4/10) Adding run script...
+(5/10) Setting ENV variables...
+(6/10) Adding mount points...
+(7/10) Fixing permissions...
+(8/10) Stopping and removing the container...
+(9/10) Building squashfs container...
+Building image from sandbox: /tmp/ubuntu_14.04-2017-09-13-3e51deeadc7b.build
+Building Singularity image...
+Singularity container built: /tmp/ubuntu_14.04-2017-09-13-3e51deeadc7b.simg
+Cleaning up...
+(10/10) Moving the image to the output folder...
+     62,591,007 100%  340.92MB/s    0:00:00 (xfr#1, to-chk=0/1)
+Final Size: 60MB
+```
+
+Notice how the image went from 188MB to 60MB? This reduction is even more impressive when we are dealing with
+very large images (e.g., ~3600 down to ~1800). A few notes on the inputs shown above that you should edit:
+
+ - `/host/path/change/me`: the path you want to have the final image reside. If you are on windows this might look like `D:\host\path\where\to\output\singularity\image`.
+ -`ubuntu:14.04`: the docker image name you wish to convert (it will be pulled from Docker Hub if it does not exist on your host system).
+
+`docker2singularity` uses the Docker daemon located on the host system. It will access the Docker image cache from the host system avoiding having to redownload images that are already present locally.
+
+If you ever need to make changes, you can easily export the squashfs image into either a sandbox folder or ext3 (legacy) image, both of which have writable.
+
+```
+sudo singularity build --sandbox sandbox/ production.simg
+sudo singularity build --writable ext3.img production.simg
+```
+
+## Inspect Your Image
+New with `docker2singularity` 2.4, the labels for the container are available with `inspect`:
+
+```
+ singularity inspect ubuntu_14.04-2017-09-13-3e51deeadc7b.simg 
+{
+    "org.label-schema.singularity.build": "squashfs",
+    "org.label-schema.docker.version": "17.06.2-ce",
+    "org.label-schema.schema-version": "1.0",
+    "org.label-schema.singularity.build-type": "docker2singularity",
+    "org.label-schema.docker.id": "sha256:dea1945146b96542e6e20642830c78df702d524a113605a906397db1db022703",
+    "org.label-schema.build-date": "2017-10-28-17:19:18",
+    "org.label-schema.singularity.version": "2.4-dist",
+    "org.label-schema.docker.created": "2017-09-13"
+}
+```
+
+as is the runscript and environment
+
+```
+singularity inspect --json -e -r ubuntu_14.04-2017-09-13-3e51deeadc7b.simg 
+{
+    "data": {
+        "attributes": {
+            "environment": "# Custom environment shell code should follow\n\n",
+            "runscript": "#!/bin/sh\n/bin/bash $@\n"
+        },
+        "type": "container"
+    }
+}
+
+```
+
+## Build a Sandbox Image
+A sandbox image is a folder that is ideal for development. You can view it on your desktop, cd inside and browse, and it works like a Singularity image. To create a sandbox, specify the `-f` flag:
+
+```
+docker run -v /var/run/docker.sock:/var/run/docker.sock \
+-v /host/path/change/me:/output \
+--privileged -t --rm \
+singularityware/docker2singularity \
+-f \
+ubuntu:14.04
+```
+Importantly, you can use `--writable`, and if needed, you can convert a sandbox folder into a production image:
+
+```
+sudo singularity build sandbox/ production.simg
+```
+
+## Build a Legacy (ext3) Image
+You can build a legacy ext3 image (with `--writable`) with the `-w` flag. This is an older image format that is more prone to degradation over time, and (building) may not be supported for future versions of the software.
+
+```
+docker run -v /var/run/docker.sock:/var/run/docker.sock \
+-v /host/path/change/me:/output \
+--privileged -t --rm \
+singularityware/docker2singularity \
+-w \
+ubuntu:14.04
+```
+You can also use `--writable` and convert an ext3 image into a production image:
+
+```
+sudo singularity build ext3.img production.simg
+```
+
+
+## Tips for making Docker images compatible with Singularity
+
+ - Define all environmental variables using the `ENV` instruction set. Do not rely on `.bashrc`, `.profile`, etc.
+ - Define an `ENTRYPOINT` instruction set pointing to the command line interface to your pipeline
+ - Do not define `CMD` - rely only on `ENTRYPOINT`
+ - You can interactively test the software inside the container by overriding the `ENTRYPOINT`
+   `docker run -i -t --entrypoint /bin/bash bids/example`
+ - Do not rely on being able to write anywhere other than the home folder and `/scratch`. Make sure your container runs with the `--read-only --tmpfs /run --tmpfs /tmp` parameters (this emulates the read-only behavior of Singularity)
+ - Don’t rely on having elevated user permissions
+ - Don’t use the USER instruction set
+
+## FAQ
+### "client is newer than server" error
+If you are getting the following error:
+`docker: Error response from daemon: client is newer than server`
+
+You need to use the `docker info` command to check your docker version and use it to grab the correct corresponding version of `docker2singularity`. For example:
+
+     docker run \        
+     -v /var/run/docker.sock:/var/run/docker.sock \
+     -v D:\host\path\where\to\output\singularity\image:/output \
+     --privileged -t --rm \
+     singularityware/docker2singularity:1.11 \            
+     ubuntu:14.04
+
+Currently only the 1.10, 1.11, 1.12, and 1.13  versions are supported. If you are using an older version of Docker you will need to upgrade.
+
+
+### My cluster/HPC requires Singularity images to include specific mount points
+If you are getting `WARNING: Non existant bind point (directory) in container: '/shared_fs'` or a similar error when running your Singularity image that means that your Singularity images require custom mount points. To make the error go away you can specify the mount points required by your system when creating the Singularity image:
+
+     docker run \        
+     -v /var/run/docker.sock:/var/run/docker.sock \
+     -v D:\host\path\where\to\output\singularity\image:/output \
+     --privileged -t --rm \
+     singularityware/docker2singularity \            
+     -m "/shared_fs /custom_mountpoint2" \
+     ubuntu:14.04
+
+
+## Acknowledgements
+This work is heavily based on the `docker2singularity` work done by [vsoch](https://github.com/vsoch) and [gmkurtzer](https://github.com/gmkurtzer). Hopefully most of the conversion code will be merged into Singularity in the future making this container even leaner!
---
+++ docker2singularity/docker2singularity.sh
@@ -0,0 +1,273 @@
+#! /bin/bash
+#
+# docker2singularity.sh will convert a docker image into a singularity
+# Must be run with sudo to use docker commands (eg aufs)
+#
+# NOTES:
+# If the docker image uses both ENTRYPOINT and CMD the latter will be ignored
+#
+# KNOWN ISSUES:
+# Currently ENTRYPOINTs and CMDs with commas in the arguments are not supported
+#
+# USAGE: docker2singularity.sh ubuntu:14.04
+#
+#
+# Copyright (c) 2016-2017 Vanessa Sochat, All Rights Reserved
+# Copyright (c) 2017 Singularityware LLC and AUTHORS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -o errexit
+set -o nounset
+
+USAGE="USAGE: docker2singularity [-m \"/mount_point1 /mount_point2\"] [options] docker_image_name"
+
+# --- Option processing --------------------------------------------
+if [ $# == 0 ] ; then
+    echo $USAGE
+    echo "OPTIONS:
+
+          Image Format
+              -f: build development sandbox (folder)
+              -w: non-production writable image (ext3)         
+
+              Default is squashfs (recommended)
+              "
+
+    exit 1;
+fi
+
+mount_points="/oasis /projects /scratch /local-scratch /work /home1 /corral-repl /corral-tacc /beegfs /share/PI /extra /data /oak"
+image_format="squashfs"
+while getopts ':hm:wf' option; do
+  case "$option" in
+    h) echo "$USAGE"
+       exit 0
+       ;;
+    m) mount_points=$OPTARG
+       ;;
+    f) image_format="sandbox"
+       ;;
+    w) image_format="writable"
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+image=$1
+
+echo ""
+echo "Image Format: ${image_format}"
+
+################################################################################
+### CONTAINER RUNNING ID #######################################################
+################################################################################
+
+runningid=`docker run -d $image tail -f /dev/null`
+
+# Full id looks like
+# sha256:d59bdb51bb5c4fb7b2c8d90ae445e0720c169c553bcf553f67cb9dd208a4ec15
+
+# Take the first 12 characters to get id of container
+container_id=`echo ${runningid} | cut -c1-12`
+
+# Network address, if needed
+network_address=`docker inspect --format="{{.NetworkSettings.IPAddress}}" $container_id`
+
+
+################################################################################
+### IMAGE NAME #################################################################
+################################################################################
+
+image_name=`docker inspect --format="{{.Config.Image}}" $container_id`
+
+# using bash substitution
+# removing special chars [perhaps echo + sed would be better for other chars]
+image_name=${image_name//\//_}
+image_name=${image_name/:/_}
+
+# following is the date of the container, not the docker image.
+#creation_date=`docker inspect --format="{{.Created}}" $container_id`
+creation_date=`docker inspect --format="{{.Created}}" $image`
+
+
+################################################################################
+### IMAGE SIZE #################################################################
+################################################################################
+
+size=`docker inspect --format="{{.Size}}" $image`
+# convert size in MB
+size=`echo $(($size/1000000+1))`
+echo "Inspected Size: $size MB"
+echo ""
+
+################################################################################
+### IMAGE CREATION #############################################################
+################################################################################
+TMPDIR=$(mktemp -u -d)
+mkdir -p $TMPDIR
+
+creation_date=`echo ${creation_date} | cut -c1-10`
+new_container_name=/tmp/$image_name-$creation_date-$container_id
+build_sandbox="${new_container_name}.build"
+echo "(1/10) Creating a build sandbox..."
+mkdir -p ${build_sandbox}
+echo "(2/10) Exporting filesystem..."
+docker export $container_id >> $build_sandbox.tar
+singularity image.import $build_sandbox < $build_sandbox.tar
+docker inspect $container_id >> $build_sandbox/singularity.json
+
+
+################################################################################
+### METADATA ###################################################################
+################################################################################
+
+# Quiet Singularity debug output when adding labels
+SINGULARITY_MESSAGELEVEL=0
+export SINGULARITY_MESSAGELEVEL
+
+# For docker2singularity, installation is at /usr/local
+echo "(3/10) Creating labels..."
+libexec="/usr/local/libexec/singularity"
+zcat "${libexec}/bootstrap-scripts/environment.tar" | ( cd $build_sandbox; tar -xf - >/dev/null)
+LABELS=$(docker inspect --format='{{json .Config.Labels}}' $image)
+LABELFILE=$(printf "%q" "$build_sandbox/.singularity.d/labels.json")
+ADD_LABEL="${libexec}/python/helpers/json/add.py -f --file ${LABELFILE}"
+
+# Labels could be null
+if [ "${LABELS}" == "null" ]; then
+    LABELS="{}"
+fi
+
+# Extract some other "nice to know" metadata from docker
+SINGULARITY_version=`singularity --version`
+SINGULARITY_VERSION=$(printf "%q" "$SINGULARITY_version")
+DOCKER_VERSION=$(docker inspect --format='{{json .DockerVersion}}' $image)
+DOCKER_ID=$(docker inspect --format='{{json .Id}}' $image)
+
+# Add labels from Docker, then relevant to Singularity build
+echo $LABELS > $LABELFILE;
+eval $ADD_LABEL --key "org.label-schema.schema-version" --value "1.0"
+eval $ADD_LABEL --key "org.label-schema.singularity.build-type" --value "docker2singularity" 
+eval $ADD_LABEL --key "org.label-schema.singularity.build" --value "${image_format}" 
+eval $ADD_LABEL --key "org.label-schema.build-date" --value $(date +%Y-%m-%d-%H:%M:%S)
+eval $ADD_LABEL --key "org.label-schema.singularity.version" --value "${SINGULARITY_VERSION}"
+eval $ADD_LABEL --key "org.label-schema.docker.version" --value "${DOCKER_VERSION}"
+eval $ADD_LABEL --key "org.label-schema.docker.Created" --value "${creation_date}"
+eval $ADD_LABEL --key "org.label-schema.docker.Id" --value "${DOCKER_ID}"
+
+unset SINGULARITY_MESSAGELEVEL
+
+################################################################################
+### SINGULARITY RUN SCRIPT #####################################################
+################################################################################
+echo "(4/10) Adding run script..."
+CMD=$(docker inspect --format='{{json .Config.Cmd}}' $image)
+if [[ $CMD != [* ]]; then
+    if [[ $CMD != "null" ]]; then
+        CMD="/bin/sh -c "$CMD
+    fi
+fi
+# Remove quotes, commas, and braces
+CMD=`echo "${CMD//\"/}" | sed 's/\[//g' | sed 's/\]//g' | sed 's/,//g'`
+
+ENTRYPOINT=$(docker inspect --format='{{json .Config.Entrypoint}}' $image)
+if [[ $ENTRYPOINT != [* ]]; then
+    if [[ $ENTRYPOINT != "null" ]]; then
+        ENTRYPOINT="/bin/sh -c "$ENTRYPOINT
+    fi
+fi
+
+# Remove quotes, commas, and braces
+ENTRYPOINT=`echo "${ENTRYPOINT//\"/}" | sed 's/\[//g' | sed 's/\]//g' | sed 's/,/ /g'`
+
+echo '#!/bin/sh' > $build_sandbox/.singularity.d/runscript
+if [[ $ENTRYPOINT != "null" ]]; then
+    echo $ENTRYPOINT '$@' >> $build_sandbox/.singularity.d/runscript;
+else
+    if [[ $CMD != "null" ]]; then
+        echo $CMD '$@' >> $build_sandbox/.singularity.d/runscript;
+    fi
+fi
+
+chmod +x $build_sandbox/.singularity.d/runscript;
+
+
+################################################################################
+### SINGULARITY ENVIRONMENT ####################################################
+################################################################################
+
+echo "(5/10) Setting ENV variables..."
+docker run --rm --entrypoint="/usr/bin/env" $image > $TMPDIR/docker_environment
+# do not include HOME and HOSTNAME - they mess with local config
+sed -i '/^HOME/d' $TMPDIR/docker_environment
+sed -i '/^HOSTNAME/d' $TMPDIR/docker_environment
+sed -i 's/^/export /' $TMPDIR/docker_environment
+# add quotes around the variable names
+sed -i 's/=/="/' $TMPDIR/docker_environment
+sed -i 's/$/"/' $TMPDIR/docker_environment
+cp $TMPDIR/docker_environment $build_sandbox/.singularity.d/env/10-docker.sh
+chmod +x $build_sandbox/.singularity.d/env/10-docker.sh;
+rm -rf $TMPDIR
+
+
+################################################################################
+### Permissions ################################################################
+################################################################################
+if [ "${mount_points}" ] ; then
+    echo "(6/10) Adding mount points..."
+    mkdir -p "${build_sandbox}/${mount_points}"
+else
+    echo "(6/10) Skipping mount points..."
+fi 
+
+# making sure that any user can read and execute everything in the container
+echo "(7/10) Fixing permissions..."
+
+find ${build_sandbox}/* -maxdepth 0 -not -path '${build_sandbox}/dev*' -not -path '${build_sandbox}/proc*' -not -path '${build_sandbox}/sys*' -exec chmod a+r -R '{}' \;
+find ${build_sandbox}/* -type f -or -type d -perm -u+x,o-x -not -path '${build_sandbox}/dev*' -not -path '${build_sandbox}/proc*' -not -path '${build_sandbox}/sys*' -exec chmod a+x '{}' \;
+
+echo "(8/10) Stopping and removing the container..."
+docker stop $container_id >> /dev/null
+docker rm $container_id >> /dev/null
+
+# Build a final image from the sandbox
+echo "(9/10) Building ${image_format} container..."
+if [ "$image_format" == "squashfs" ]; then
+    new_container_name=${new_container_name}.simg
+    singularity build ${new_container_name} $build_sandbox
+elif [ "$image_format" == "writable" ]; then
+    new_container_name=${new_container_name}.img    
+    singularity build --writable ${new_container_name} $build_sandbox
+else
+    mv $build_sandbox $new_container_name
+fi
+
+echo "(10/10) Moving the image to the output folder..."
+finalsize=`du -shm $new_container_name | cut -f1`
+rsync --info=progress2 -a $new_container_name /output/
+echo "Final Size: ${finalsize}MB"
